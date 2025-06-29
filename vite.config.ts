bd2f18d59de34ce193d5df7f271eb751cbeb7a1a
Harysson Soares <haryssonsoares@gmail.com>
2025-06-29 00:28:31 -0300

    feat: bump version to 1.0.6 in package.json and package-lock.json; update vite.config.ts to include react-hook-form as an external dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import path from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -12,11 +11,12 @@ export default defineConfig({
       fileName: (format) => `rharuow-ds.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react", "react-dom", "react-hook-form"],
       output: {
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
+          "react-hook-form": "ReactHookForm",
         },
       },
     },
