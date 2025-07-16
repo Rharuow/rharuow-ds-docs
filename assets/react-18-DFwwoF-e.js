06ed5ff5c0aa86eb50be7120ad6ea5bbf48f002e
Harysson Soares <haryssonsoares@gmail.com>
2025-07-15 22:21:28 -0300

    Updates

--- assets/react-18-DFwwoF-e.js
+++ assets/react-18-DFwwoF-e.js
@@ -1,4 +1,4 @@
-import{a as k1,r as Fc}from"./iframe-NESEoKY2.js";var wc={exports:{}},ie={},Wc={exports:{}},$c={};/**
+import{a as k1,r as Fc}from"./iframe-CYbMvA8f.js";var wc={exports:{}},ie={},Wc={exports:{}},$c={};/**
  * @license React
  * scheduler.production.js
  *
